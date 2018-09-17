fbef984dc4671715720ebf1ac88cc37be9cfedc4
Igor Shishkin <me@teran.ru>
2018-09-17 22:15:52 +0300

    Update release in Homebrew formula
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- contrib/Homebrew/checksum.rb
+++ contrib/Homebrew/checksum.rb
@@ -1,9 +1,9 @@
 class Checksum < Formula
   desc "SHA256 file verification for consistency check purposes"
   homepage "https://github.com/teran/checksum"
-  version "0.5"
+  version "0.6"
   url "https://github.com/teran/checksum/archive/v#{version}.tar.gz"
-  sha256 "aa5a7c50f549c4142ae56841c427edfbdd649259a7759fd05a243bf9f0bffb91"
+  sha256 "0f77ac41bf42c9566f4a3e907de74898c2bfeb80449cefd4ef25c1252a875467"
 
   depends_on "go" => :build
   depends_on "make" => :build
